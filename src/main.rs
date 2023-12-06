ba34441edb25eac96a2f446cbb811a5635785ea8
Washiil <65116802+Washiil@users.noreply.github.com>
2023-12-06 00:03:54 -0500

    updated benchmark outputs

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,17 @@ mod utility;
 use crate::runner::Runner;
 
 fn main() {
-
     let days: Vec<Box<dyn Runner>> = vec![
         Box::new(days::day01::Day01),
         Box::new(days::day02::Day02),
         Box::new(days::day03::Day03)
     ];
 
+    println!("---------------------------------------------------");
+    println!("Day    | Value 1        | Value 2        | Duration");
+    println!("---------------------------------------------------");
     for (i, day) in days.iter().enumerate() {
         let duration = day.benchmark();
-        println!("Day {} | {:?}", i + 1, duration);
+        println!("Day {:0>2} | {: <14} | {: <14} | {:?}", i + 1, &duration.0, &duration.1, &duration.2);
     }
 }
