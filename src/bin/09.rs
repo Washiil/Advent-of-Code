76526b4c28cc6a91034df4ea79d5c39d55081a84
Washiil <65116802+Washiil@users.noreply.github.com>
2023-12-13 16:34:31 -0500

    Advent of code day 9 Part 1 and 2

--- src/bin/09.rs
+++ src/bin/09.rs
@@ -1,41 +1,68 @@
 advent_of_code::solution!(9);
 
-fn get_differences(input: Vec<u32>) -> Vec<u32> {
-    let differences: Vec<u32> = 
-            input
-                .windows(2)
-                .map(|pair| pair[0] - pair[1])
-                .collect();
+fn get_differences(input: Vec<i32>) -> Vec<i32> {
+    input
+        .windows(2)
+        .map(|pair| pair[1] - pair[0])
+        .collect()
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
     let history = input.lines();
+    let mut total = 0;
 
     for sequence in history {
-        let values: Vec<u32> = 
+        let values: Vec<i32> = 
             sequence
                 .split(" ")
                 .filter_map(|n| n.parse().ok())
                 .collect();
-        
-        let mut sequences: Vec<Vec<u32>> = Vec::new();
 
-        let differences: Vec<u32> = 
-            values
-                .windows(2)
-                .map(|pair| pair[0] - pair[1])
-                .collect();
+        let mut sequences: Vec<Vec<i32>> = vec![values.clone()];
         
-        while differences.iter().all(|&x| x == values[0]) {
-            sequences.push(differences.clone())
+        let mut current_differences = get_differences(values);
+        while !current_differences.iter().all(|&x| x == 0) {
+            sequences.push(current_differences.clone());
+            current_differences = get_differences(current_differences);
+        }
+
+        for s in sequences {
+            total += s.last().unwrap();
         }
     }
-
-    None
+    Some(total as u32)
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<i32> {
+    let history = input.lines();
+    let mut total: i32 = 0;
+
+    for sequence in history {
+        let values: Vec<i32> = 
+            sequence
+                .split(" ")
+                .filter_map(|n| n.parse().ok())
+                .collect();
+
+        let mut sequences: Vec<Vec<i32>> = vec![values.clone()];
+        
+        let mut current_differences = get_differences(values);
+        while !current_differences.iter().all(|&x| x == 0) {
+            sequences.push(current_differences.clone());
+            current_differences = get_differences(current_differences);
+        }
+        
+        sequences.push(vec![0; current_differences.len()]);
+        sequences.reverse();
+
+        for i in 0..sequences.len() - 1 {
+            let x = sequences[i + 1][0] - sequences[i][0];
+            sequences[i + 1].insert(0, x);
+        }
+
+        total += sequences.last().unwrap().first().unwrap();
+    }
+    Some(total)
 }
 
 #[cfg(test)]
@@ -45,12 +72,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(114));
     }
 
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(2));
     }
 }
