b27f7676977b0fce6bb2d0a5cfc8477dd600ccb0
Washi <65116802+Washiil@users.noreply.github.com>
2024-12-06 01:10:10 -0500

    2024 Day 06 Part B

--- 2024/src/bin/06.rs
+++ 2024/src/bin/06.rs
@@ -81,7 +81,63 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let mut output = 0;
+    let mut map: Vec<Vec<char>> = Vec::new();
+
+    let mut starting_position: (i32, i32) = (0, 0);
+    let starting_direction: Direction = Direction::Up;
+
+    for (row, line) in input.lines().enumerate() {
+        if line.contains('^') {
+            for (col, c) in line.chars().enumerate() {
+                if c == '^' {
+                    starting_position = (col as i32, row as i32)
+                }
+            }
+        }
+        map.push(line.chars().collect::<Vec<char>>());
+    }
+
+    for i in 0..map.len() {
+        for j in 0..map[0].len() {
+            if map[i][j] == '^' || map[i][j] == '#' {
+                continue;
+            }
+
+            map[i][j] = '#';
+
+            let mut visited: HashSet<((i32, i32), (i32, i32))> = HashSet::new();
+            let mut direction = starting_direction.clone();
+            let mut position = starting_position;
+
+            while let Some(&character) = map.get(position.1 as usize).and_then(|row| row.get(position.0 as usize)) {
+                let cardinal = direction.to_cardinal();
+
+                if character == '#' {
+                    position.0 -= cardinal.0;
+                    position.1 -= cardinal.1;
+                    direction.turn_right();
+                }
+                else {
+                    if visited.contains(&(position, cardinal)) {
+                        output += 1;
+                        break;
+                    }
+                    else {
+                        visited.insert((position, cardinal));
+            
+                        position.0 += cardinal.0;
+                        position.1 += cardinal.1;
+                    }
+                }
+            }
+
+            visited.clear();
+            map[i][j] = '.';
+        }
+    }
+
+    Some(output)
 }
 
 #[cfg(test)]
