9f11508504b128750049f11eb17d5f5abb5386a9
Washi <65116802+Washiil@users.noreply.github.com>
2025-12-01 20:03:30 -0500

    2025 Day 1 Part B

--- 2025/src/bin/01.rs
+++ 2025/src/bin/01.rs
@@ -1,5 +1,5 @@
 use core::panic;
-use std::i32;
+use std::{i32, path::PathBuf};
 
 advent_of_code::solution!(1);
 
@@ -30,12 +30,9 @@ pub fn part_one(input: &str) -> Option<u64> {
 
         // 2. Parse the owned String
         let dist = dist_str.parse::<i16>().unwrap();
-
-        let prev_dial = dial;
-
         match direction {
-            Some('L') => dial = (dial + dist) % 100,
-            Some('R') => dial = (dial - dist) % 100,
+            Some('L') => dial = (dial - dist) % 100,
+            Some('R') => dial = (dial + dist) % 100,
             Some(_) => panic!("Unreachable"),
             None => panic!("Unreachable"),
         }
@@ -49,7 +46,56 @@ pub fn part_one(input: &str) -> Option<u64> {
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let lines = input.split('\n');
+
+    let mut dial: i32 = 50;
+    let mut zeros = 0;
+
+    for line in lines {
+        let mut chars = line.chars();
+        
+        // Parse out the direction maybe make an enum later if im feeling fancy
+        let direction = match chars.next() {
+            Some(c) => c,
+            None => continue,
+        };
+        
+        // Parse distance 
+        let dist_str: String = chars.take_while(|x| x.is_numeric()).collect();
+        if dist_str.is_empty() { 
+            panic!("Unreachable / Bad Input!")
+        }
+        
+        let dist = dist_str.parse::<i32>().unwrap();
+
+        match direction {
+            'R' => {
+                zeros += (dial + dist) as u64 / 100;
+                dial = (dial + dist) % 100;
+            }
+            
+            'L' => {
+                // I love ternary expressions
+                let dist_to_zero = if dial == 0 { 100 } else { dial };
+
+                if dist >= dist_to_zero {
+                    let mut count = 1;
+                    
+                    // We add one more zero for every subsequent hundred "clicks"
+                    let remaining_dist = dist - dist_to_zero;
+                    count += remaining_dist / 100;
+                    
+                    zeros += count as u64;
+                }
+
+                // Rust % operator does not actually work like a mathematical mod on negative numbers
+                dial = (dial - dist).rem_euclid(100);
+            }
+            _ => panic!("Unknown direction"),
+        }
+    }
+
+    Some(zeros)
 }
 
 #[cfg(test)]
@@ -65,6 +111,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(6));
     }
 }
